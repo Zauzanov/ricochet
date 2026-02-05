8d1eeadef8b4fc200e3b32c71d1dce9e85220560
Zauzanov <zauzanov@gmail.com>
2026-02-05 12:43:25 +0300

    Create a file with custom headers

---
+++ urllib2-python2-flexible.py
@@ -0,0 +1,8 @@
+import urllib2                                      # Python 2's HTTP client lib for opening URLs.
+url = 'https://example.com'                         # URL we want to request.
+headers = {'User-agent': 'Googlebot'}               # We specify our own headers. Web crawler used by Google to discover web pages.
+request = urllib2.Request(url, headers=headers)     # We pass the URL and the dict to it. 
+response = urllib2.urlopen(request)                 # Pass the new-established object to the func. Performs an HTTP request, GET by default. Returns a file-like response object.  
+print(response.read())                              # Reads the entire response body from the server as bytes, then prints it.
+response.close()                                    # Close the response and underlying socket to free resources.
+
