d10ec64c72b6af13d15b92f886be65156d6cb366
Zauzanov <zauzanov@gmail.com>
2026-02-06 19:19:50 +0300

    Add comments

--- urllib-python3-get.py
+++ urllib-python3-get.py
@@ -2,7 +2,7 @@ import urllib.parse
 import urllib.request
 
 url = 'https://example.com/'
-with urllib.request.urlopen(url) as response: # GET
+with urllib.request.urlopen(url) as response:           # We use urlopen() as a context manager, making a GET-request, then reading it.
     content = response.read()
 
 print(content)
