bab416e5ab57c9bf619d732728d5e8d9942dacfa
Zauzanov <zauzanov@gmail.com>
2026-02-06 18:59:11 +0300

    Fix TLS certificate verification

--- urllib2-python2.py
+++ urllib2-python2.py
@@ -1,5 +1,6 @@
 import urllib2                                      # Python 2's HTTP client lib for opening URLs.
-url = 'https://example.com'                         # URL we want to request.
+
+url = 'http://example.com'                          # URL we want to request.
 response = urllib2.urlopen(url)                     # Performs an HTTP request, GET by default. Returns a file-like response object.  
 print(response.read())                              # Reads the entire response body from the server as bytes, then prints it.
 response.close()                                    # Close the response and underlying socket to free resources.
