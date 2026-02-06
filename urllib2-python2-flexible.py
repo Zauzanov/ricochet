667b172dcbe4325e23d755965050dc5ba4102bc5
Zauzanov <zauzanov@gmail.com>
2026-02-06 19:03:32 +0300

    Fix TLS certificate verification

--- urllib2-python2-flexible.py
+++ urllib2-python2-flexible.py
@@ -1,5 +1,6 @@
 import urllib2                                      # Python 2's HTTP client lib for opening URLs.
-url = 'https://example.com'                         # URL we want to request.
+
+url = 'http://example.com'                          # URL we want to request.
 headers = {'User-agent': 'Googlebot'}               # We specify our own headers. Web crawler used by Google to discover web pages.
 request = urllib2.Request(url, headers=headers)     # We pass the URL and the dict to it. 
 response = urllib2.urlopen(request)                 # Pass the new-established object to the func. Performs an HTTP request, GET by default. Returns a file-like response object.  
