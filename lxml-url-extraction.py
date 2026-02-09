7957317a0f61b0451da3197bbef80309da008c09
Zauzanov <zauzanov@gmail.com>
2026-02-09 19:08:02 +0300

    Add comments

--- lxml-url-extraction.py
+++ lxml-url-extraction.py
@@ -1,3 +1,12 @@
 from io import BytesIO
 from lxml import etree
+import requests
 
+url = 'https://example.com'
+r = requests.get(url)                                   # GET-request.
+content = r.content                                     # `content` is a byte data type.
+
+parser = etree.HTMLParser()
+content = etree.parse(BytesIO(content), parser=parser)  # Converts into a tree. 
+for link in content.findall('//a'):                     # Finds all URLs — elements 'a'.
+    print(f"{link.get('href')} -> {link.text}")
