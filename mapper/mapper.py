c5c5da4865871071e519f1c10c9599abdae17e3b
Zauzanov <zauzanov@gmail.com>
2026-02-11 01:33:41 +0300

    Create mapper.py

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -6,3 +6,21 @@ import sys
 import threading
 import time
 
+FILTERED = [".jpg", ".gif", ".png", ".css"]
+TARGET = "https://example.com/wordpress"
+THREADS = 10
+
+answers = queue.Queue()
+web_paths = queue.Queue()
+
+def gather_paths():
+    for root, _, files in os.walk('.'):
+        for fname in files:
+            if os.path.splitext(fname)[1] in FILTERED:
+                continue
+            path = os.path.join(root, fname)
+            if path.startswith('.'):
+                path = path[1:]
+            print(path)
+            web_paths.put(path)
+
