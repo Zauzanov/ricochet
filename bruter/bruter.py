51ca91ae966f101434def4387e22dfa343657fda
Zauzanov <zauzanov@gmail.com>
2026-02-18 07:39:05 +0300

    Add get_words()

--- bruter/bruter.py
+++ bruter/bruter.py
@@ -9,3 +9,32 @@ TARGET = "http://localhost:8080"
 THREADS = 50
 WORDLIST = "/home/kali/Downloads/all.txt"
 
+def get_words(resume=None):
+
+    def extend_words(word):
+        if "." in word:
+            word.put(f'/{word}')
+        else:
+            word.put(f'/{word}/')
+        
+        for extension in EXTENSTIONS:
+            word.put(f'/{word}{extension}')
+    
+    with open(WORDLIST) as f:
+        raw_words = f.read()
+
+    found_resume = False
+    words = queue.Queue()
+    for word in raw_words.split():
+        if resume is not None:
+            if found_resume:
+                extend_words(word)
+            elif word == resume:
+                found_resume = True
+                print(f'Resuming wordlist from: {resume}')
+        
+        else:
+            print(word)
+            extend_words(word)
+    return words 
+
