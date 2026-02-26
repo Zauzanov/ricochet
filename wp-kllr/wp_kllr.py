e5067d01755d2e89ab35b93133bf33c282dd3f50
Zauzanov <zauzanov@gmail.com>
2026-02-27 01:25:05 +0300

    Add get_words func

--- wp-kllr/wp_kllr.py
+++ wp-kllr/wp_kllr.py
@@ -10,3 +10,14 @@ import time
 SUCCESS = 'Welcome to WordPress!'
 TARGET = "http://localhost:8080/wp-login.php"
 WORDLIST = "/home/kali/Downloads/cain-and-abel.txt" 
+
+def get_words():
+    with open(WORDLIST) as f:
+        raw_words = f.read()
+    
+    words = Queue()
+
+    for word in raw_words.split():
+        words.put(word)
+    return words
+
